95b555b405054eeef241f3c8becabcc1f7edc3e5
ikrav <>
2011-07-18 17:32:09 +0000

    Commented out several functions that do not compile anymore in ROOT

--- DrellYan/Include/MyTools.hh
+++ DrellYan/Include/MyTools.hh
@@ -9,8 +9,8 @@
 
 namespace toolbox 
 {
-Double_t calcEff(const Int_t    pass, const Int_t    total, Double_t *errl=0, Double_t *errh=0, Int_t method=0);
-Double_t calcEff(const Double_t pass, const Double_t total, Double_t *errl=0, Double_t *errh=0, Int_t method=0);
+// Double_t calcEff(const Int_t    pass, const Int_t    total, Double_t *errl=0, Double_t *errh=0, Int_t method=0);
+// Double_t calcEff(const Double_t pass, const Double_t total, Double_t *errl=0, Double_t *errh=0, Int_t method=0);
 
 Double_t deltaR(const Double_t eta1, const Double_t phi1, const Double_t eta2, const Double_t phi2);
 
@@ -20,42 +20,42 @@ Int_t roundToInt(const Double_t x);
 }
 
 //------------------------------------------------------------------------------------------------------------------------
-Double_t toolbox::calcEff(Int_t pass, Int_t total, Double_t *errl, Double_t *errh, Int_t method)
-{
-  // method: 0 -> Bayes Divide
-  //         1 -> Feldman-Cousins 
-  //         2 -> Clopper-Pearson
+// Double_t toolbox::calcEff(Int_t pass, Int_t total, Double_t *errl, Double_t *errh, Int_t method)
+// {
+//   // method: 0 -> Bayes Divide
+//   //         1 -> Feldman-Cousins 
+//   //         2 -> Clopper-Pearson
   
-  Double_t r = (total>0) ? (Double_t)pass/(Double_t)total : 0;
-  if(errl) *errl = 0;
-  if(errh) *errh = 0;
+//   Double_t r = (total>0) ? (Double_t)pass/(Double_t)total : 0;
+//   if(errl) *errl = 0;
+//   if(errh) *errh = 0;
     
-  const Double_t conf = 0.68269;
+//   const Double_t conf = 0.68269;
   
-  if(method==0) {    
-    TGraphAsymmErrors g;
-    Double_t mode, low, high;
-    g.Efficiency(pass,total,conf,mode,low,high);
-    if(errl) *errl = mode - low;
-    if(errh) *errh = high - mode;
-  }
+//   if(method==0) {    
+//     TGraphAsymmErrors g;
+//     Double_t mode, low, high;
+//     g.Efficiency(pass,total,conf,mode,low,high);
+//     if(errl) *errl = mode - low;
+//     if(errh) *errh = high - mode;
+//   }
 
-  if(method==1) {
-//    FeldmanCousins fc;
-//    fc.SetConfidenceLevel(conf);
-  }
+//   if(method==1) {
+// //    FeldmanCousins fc;
+// //    fc.SetConfidenceLevel(conf);
+//   }
 
-  if(method==2) {
-  }
+//   if(method==2) {
+//   }
  
-  return r;
-}
+//   return r;
+// }
 
-Double_t toolbox::calcEff(Double_t pass, Double_t total, Double_t *errl, Double_t *errh, Int_t method) 
-{
-  // Round values to whole numbers first
-  return calcEff(roundToInt(pass),roundToInt(total),errl,errh,method);
-}
+// Double_t toolbox::calcEff(Double_t pass, Double_t total, Double_t *errl, Double_t *errh, Int_t method) 
+// {
+//   // Round values to whole numbers first
+//   return calcEff(roundToInt(pass),roundToInt(total),errl,errh,method);
+// }
 
 //------------------------------------------------------------------------------------------------------------------------
 Double_t toolbox::deltaR(Double_t eta1, Double_t phi1, Double_t eta2, Double_t phi2)
