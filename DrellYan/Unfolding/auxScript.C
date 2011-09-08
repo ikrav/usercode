e825f05910c79e6f0b3f308035586d04692313be
eavdeeva <>
2011-09-08 20:28:45 +0000

    now plotDYUnfoldingMatrix is done another 20 times with different last parameter and calcUnfoldingSystematics is done

--- DrellYan/Unfolding/auxScript.C
+++ DrellYan/Unfolding/auxScript.C
@@ -1,4 +1,10 @@
 {
   gROOT->ProcessLine(".L plotDYUnfoldingMatrix.C+");
   plotDYUnfoldingMatrix("../config_files/summer11mc.input");
+  for (int i=1; i<=20; i++)
+  {
+    plotDYUnfoldingMatrix("../config_files/summer11mc.input",true,1000+i);
+  }
+  gROOT->ProcessLine(".L calcUnfoldingSystematics.C+");
+  calcUnfoldingSystematics("../config_files/xsecCalc.conf")
 }
