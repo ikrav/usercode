42dcbc857ab3174a423165ef534cc65fdac54652
ikrav <>
2011-10-02 22:36:09 +0000

    bug fix

--- DrellYan/Fsr/auxScript2.C
+++ DrellYan/Fsr/auxScript2.C
@@ -1,4 +1,4 @@
 {
   gROOT->ProcessLine(".L plotDYFSRCorrectionsSansAcc.C+");
-  plotDYFSRCorrectionsSansAcc("../config_files/summer11mc.input")
+  plotDYFSRCorrectionsSansAcc("../config_files/summer11mc.input");
 }
