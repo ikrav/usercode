e6c2372f9eb3030d616cb706bb4d6cd34ea3d363
eavdeeva <>
2011-10-03 19:28:59 +0000

    *** empty log message ***

--- DrellYan/Acceptance/auxScript.C
+++ DrellYan/Acceptance/auxScript.C
@@ -1,6 +1,15 @@
 {
   gROOT->ProcessLine(".L plotDYAcceptance.C+");
+
+
   plotDYAcceptance("../config_files/summer11mc.input");
   plotDYAcceptance("../config_files/summer11mc.input",2,1.05);
   plotDYAcceptance("../config_files/summer11mc.input",2,0.95);
+
+
+  gROOT->ProcessLine(".L calcAcceptanceSystematics.C+");
+  calcAcceptanceSystematics("../config_files/xsecCalc.conf");
+
+  
+
 }
---
+++ DrellYan/Acceptance/calcAcceptanceSystematics.C
@@ -0,0 +1,163 @@
+#include "TVectorD.h"
+#include "TFile.h"
+#include "TString.h"
+#include "TSystem.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "../Include/DYTools.hh"
+#include "../Include/UnfoldingTools.hh"
+
+// The global variables will be used in several functions:
+TString tagDirYields = "";
+TString tagDirConstants = "";
+Double_t lumi = 0;
+
+//void readData(TVectorD &v, TVectorD &vErr1, TVectorD &vErr2);
+
+void  extractAcceptance(TVectorD &vout, int reweightInt);
+
+const TString fileDataYields("yields_bg-subtracted.root");
+
+const TString fileAcceptanceConstantsBaseReweight("acceptance_constants_reweight_");
+
+void calcAcceptanceSystematics(const TString conf){
+
+  // First, read the configuration file. The configuration file
+  // is the same as the one used for the cross section calculation
+  // script, we need to know the location of data yields and
+  // unfolding matrices
+
+  ifstream ifs;
+  ifs.open(conf.Data());
+  assert(ifs.is_open());
+  std::string line;
+  int state = 0;
+  while(getline(ifs,line)) {
+    if(line[0]=='#') continue;
+    if(state==0){
+      stringstream ss1(line); ss1 >> lumi;
+      state++;
+    }else if(state==1){
+      tagDirYields = TString(line);
+      state++;
+    }else if(state==2){
+      tagDirConstants = TString(line);
+      break;
+    }
+  }
+  ifs.close();
+
+/*
+  TVectorD signalYields(nMassBins);
+  TVectorD signalYieldsStatErr(nMassBins);
+  TVectorD signalYieldsSystErr(nMassBins);
+
+  // Read data yields from file
+  readData(signalYields, signalYieldsStatErr, signalYieldsSystErr);  
+ */ 
+  /////////////////////////////////
+  //calculate Fsr systematics 
+  /////////////////////////////////
+
+  TVectorD accFsrMax(nMassBins);
+  TVectorD accFsrMin(nMassBins);
+  TVectorD accSystPercentFsr(nMassBins);
+
+
+  extractAcceptance(accFsrMax, 105);
+  extractAcceptance(accFsrMin, 95);
+
+  for(int i = 0; i < nMassBins; i++){
+    accSystPercentFsr[i] = 100.0*fabs(accFsrMax[i]-accFsrMin[i])/(accFsrMax[i]+accFsrMin[i]);
+  }
+
+  //printing out to the screen
+
+  printf("            <acc>  rel-err-percent-Fsr\n");
+  for(int i=0; i<nMassBins; i++){
+    printf("%4.0f-%4.0f   %1.3f   %1.3f \n", 
+           massBinLimits[i], massBinLimits[i+1],  
+           (accFsrMax[i]+accFsrMin[i])/2, accSystPercentFsr[i]);
+  }
+
+
+  // Store constants in the file
+  TString outputDir(TString("../root_files/systematics/")+tagDirConstants);
+  gSystem->mkdir(outputDir,kTRUE);
+  TString unfoldingSystFileName(outputDir+TString("/acceptance_systematics.root"));
+
+  TFile fa(unfoldingSystFileName,"recreate");
+  accSystPercentFsr.Write("accSystPercent");
+  fa.Close();
+
+  return;
+}
+
+/*
+//-----------------------------------------------------------------
+// Read data
+//-----------------------------------------------------------------
+void readData(TVectorD &v, TVectorD &vErr1, TVectorD &vErr2){
+
+  printf("Load data yields\n"); fflush(stdout);
+  TFile fileYields   (TString("../root_files/yields/")+tagDirYields+TString("/")+fileDataYields);
+  TVectorD YieldsSignal       = *(TVectorD *)fileYields.FindObjectAny("YieldsSignal");
+  TVectorD YieldsSignalErr    = *(TVectorD *)fileYields.FindObjectAny("YieldsSignalErr");
+  TVectorD BinLimitsForYields = *(TVectorD *)fileYields.FindObjectAny("BinLimitsForYields");
+
+  // Check that the binning is consistent
+  bool checkResult = true;
+  if( v.GetNoElements() != nMassBins ) checkResult = false;
+  if( YieldsSignal.GetNoElements() != nMassBins ) checkResult = false;
+  for(int i=0; i<nMassBins+1; i++){
+    if( massBinLimits[i] != BinLimitsForYields[i] )
+      checkResult = false;
+  }
+  if( !checkResult ){
+    printf("ERROR: inconsistent binning in the inputs\n");
+    assert(0);
+  }else
+    printf("readData: Binning in the inputs is consistent\n");
+
+  for(int i=0; i<nMassBins; i++){
+    v[i] = YieldsSignal[i];
+    vErr1[i] = YieldsSignalErr[i];
+    // Presently zero, but in the future non-zero
+    // (e.g. background systematics, or energy scale correction effect on yields)
+    vErr2[i] = 0;
+  } 
+
+  fileYields.Close();
+  return;
+}
+*/
+//-----------------------------------------------------------------
+// Acceptance
+//-----------------------------------------------------------------
+void  extractAcceptance(TVectorD &vout, int reweightInt)
+{
+
+  // Read unfolding constants
+  printf("acc: Load constants\n"); fflush(stdout);
+
+
+  // Construct file names
+  TString fullAcceptanceConstFileName = TString("../root_files/systematics/")+tagDirConstants+TString("/");
+  fullAcceptanceConstFileName += fileAcceptanceConstantsBaseReweight;    
+  fullAcceptanceConstFileName += reweightInt;
+  fullAcceptanceConstFileName += ".root";
+  printf("Apply acceptace using constants from %s\n", fullAcceptanceConstFileName.Data());
+ 
+  TFile fAcc(fullAcceptanceConstFileName);
+  TVectorD* acc = (TVectorD*)fAcc.Get("acceptanceArray");
+  double* accArr=acc->GetMatrixArray();
+
+  for (int i=0; i<DYTools::nMassBins; i++){
+   vout[i]=accArr[i];
+  }  
+
+  return;
+}
+
