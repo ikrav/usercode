706695bc51421dd32b92de4f93cab076d30a96ee
eavdeeva <>
2011-10-04 01:21:53 +0000

    enum usage for syst mode is used

--- DrellYan/Acceptance/auxScript.C
+++ DrellYan/Acceptance/auxScript.C
@@ -3,8 +3,8 @@
 
 
   plotDYAcceptance("../config_files/summer11mc.input");
-  plotDYAcceptance("../config_files/summer11mc.input",2,1.05);
-  plotDYAcceptance("../config_files/summer11mc.input",2,0.95);
+  plotDYAcceptance("../config_files/summer11mc.input",DYTools::FSR_STUDY,1.05);
+  plotDYAcceptance("../config_files/summer11mc.input",DYTools::FSR_STUDY,0.95);
 
 
   gROOT->ProcessLine(".L calcAcceptanceSystematics.C+");
