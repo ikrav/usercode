388e9f54086d0a9ee89c2ab7a8af9c27f9cd4e5e
andriusj <>
2012-02-07 19:32:13 +0000

    superceded by evaluateESF.sh

--- DrellYan/EfficiencyScaleFactors/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# !! This script will be replaced by evaluateESF.sh
-
-#timeStamp="-`date +%Y%m%d-%H%M`"
-root -b -q -l auxScriptAdv.C+         | tee out${timeStamp}-09-auxScript-efficiencyScaleFactors.log
